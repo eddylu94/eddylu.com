f726561dcce701b83a49b1dcce6eb52b943ca0db
Eddy Lu <eddy.lu@mail.mcgill.ca>
2017-12-25 20:54:45 -0800

    Configure footer to display current year

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
 <div id="footer">
-    &copy; 2014-2016 Eddy Lu | <a href="statistics.php">Statistics</a>
+    &copy; 2014-<span id="footer_year"></span> Eddy Lu | <a href="statistics.php">Statistics</a>
+    <script>
+        var footerYear = document.getElementById("footer_year");
+        footerYear.innerHTML = (new Date()).getFullYear();
+    </script>
 </div>
