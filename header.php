4288e551edd88af56397aeff21f955d80bd80f97
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-02-12 01:59:46 -0800

    Update structure of header

--- header.php
+++ header.php
@@ -3,20 +3,29 @@
 <?php include 'statRecorder.php';?>
   
 <div id="header">
-    <div id="header_wrapper">
-        <a href="index.php">
-            <div id="title">
-                <div id="title_left">Eddy&nbsp;</div><div id="title_right">Lu</div>
-            </div>
-        </a>
-        <div id="menu">
-            <div id="menu_left">
-                <ul>
-                    <li><a href="education.php">Resume</a></li>
-                    <li><a href="./notes/Home.php">Notes</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
+    <div id="header_bar">
+        <div id="header_wrapper">
+            <a href="index.php">
+                <div id="title">
+                    <div id="title_left">Eddy&nbsp;</div><div id="title_right">Lu</div>
+                </div>
+            </a>
+            <div id="menu">
+                <div id="menu_left">
+                    <ul>
+                        <li><a href="education.php">Resume</a></li>
+                        <!-- <li>
+                            <ul>
+                                <li><a href="education.php">Education</a></li>
+                                <li><a href="experience.php">Experience</a></li>
+                            </ul>
+                        </li> -->
+                        <li><a href="./notes/Home.php">Notes</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<div id="header_placeholder"></div>
