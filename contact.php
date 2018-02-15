8a3594d4133794592ff99ba4bb8cb23665f2fe9a
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-02-15 00:18:47 -0800

    Change UI of contact form

--- contact.php
+++ contact.php
@@ -6,10 +6,47 @@
     <title>Eddy Lu</title>
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <style>
+        #contactForm {
+            margin: 20px auto 0px auto;
+
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+            padding: 20px;
+            border: 1px solid #CCCCCC;
+        }
+
+        .contactForm_textInput {
+            margin-bottom: 10px;
+
+            width: 100%;
+            padding: 5px;
+            box-sizing: border-box;
+            border: 1px solid #CCCCCC;
+            border-radius: 5px;
+            
+            font-size: x-large;
+            font-family: Calibri;
+        }
+
+        #contactForm_textArea {
+            margin-top: 10px;
+            margin-bottom: 20px;
+
+            width: 100%;
+            padding: 5px;
+            box-sizing: border-box;
+            border: 1px solid #CCCCCC;
+            border-radius: 5px;
+            
+            resize: vertical;
+            font-size: x-large;
+            font-family: Calibri;
+        }
+
         #contactForm_button {
             height: 50px;
-            width: 710px;
-            margin: 30px 0px 10px 0px;
+            width: 100%;
             padding: 10px;
             border-radius: 5px;
             border: none;   
@@ -36,14 +73,11 @@
             <div id="body_wrapper">
                 <div id="container" style="text-align: center;">
                     <h2 style="color: #333333;">Have a question or comment? Feel free to shoot me a message!</h2>
-                    <form action="contactForm.php" method="post" style="margin-left: auto; margin-right: auto; display: inline-block; background-color: #CCCCCC; border-radius: 5px; margin-top: 20px; padding: 20px;">
-                        <input type="text" name="firstName" placeholder="First Name" autofocus style="border-radius: 5px; border: 0; width: 240px; margin: 0px 5px 0px 0px; padding: 5px; font-size: x-large; font-family: Calibri;" />
-                        <input type="text" name="lastName" placeholder="Last Name" style="border-radius: 5px; border: 0; width: 440px; margin: 0px 0px 0px 0px; padding: 5px; font-size: x-large; font-family: Calibri;" />
-                        <br />
-                        <input type="text" name="email" placeholder="Your Email" style="border-radius: 5px; border: 0; width: 700px; margin: 10px 0px 0px 0px; font-size: x-large; padding: 5px; font-family: Calibri;" />
-                        <br />
-                        <textarea rows="6" name="message" placeholder="Your message here!"  style="border-radius: 5px; border: 0; width: 700px; margin: 30px 0px 0px 0px; font-size: x-large; padding: 5px; font-family: Calibri;"></textarea>
-                        <br>
+                    <form id="contactForm" action="contactForm.php" method="post">
+                        <input class="contactForm_textInput" type="text" name="firstName" placeholder="First Name" autofocus />
+                        <input class="contactForm_textInput" type="text" name="lastName" placeholder="Last Name" />
+                        <input class="contactForm_textInput" type="text" name="email" placeholder="Email Address" />
+                        <textarea id="contactForm_textArea" rows="6" name="message" placeholder="Your message here!"></textarea>                    
                         <input id="contactForm_button" type="submit" name="submit" value="Submit" />
                     </form>
                 </div>
