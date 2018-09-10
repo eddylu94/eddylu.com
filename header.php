96a649363b9d39863406be619c705969f09f5d5f
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-09-09 22:05:01 -0700

    Implement mobile reflow for header

--- header.php
+++ header.php
@@ -1,11 +1,47 @@
 <link rel="stylesheet" type="text/css" href="header.css">
 
 <?php include 'statRecorder.php';?>
-  
+
+<script src="reflow.js"></script>
+
+<div id="menu_flyout_overlay" onclick="toggleFlyout()"></div>
+<div id="menu_flyout">
+    <ul>
+        <li>
+            <a href="experience.php">Resume</a>
+            <ul>
+                <li><a href="experience.php">Experience</a></li>
+                <li><a href="education.php">Education</a></li>
+            </ul>
+        </li>
+        <li><a href="./notes/Home.php">Notes</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+
+    <div id="socialIcons">
+        <a href="https://facebook.com/eddylu94" target="_blank">
+            <img id="socialIcon" alt="Facebook" src="fb-icon_round.png" />
+        </a>
+        <a href="https://twitter.com/eddylu94" target="_blank">
+            <img id="socialIcon" alt="Twitter" src="tw-icon_round.png" />
+        </a>
+        <a href="https://instagram.com/eddylu94" target="_blank">
+            <img id="socialIcon" alt="Instagram" src="ig-icon_round.png" />
+        </a>                    
+        <a href="https://linkedin.com/in/lueddy" target="_blank">
+            <img id="socialIcon" alt="LinkedIn" src="in-icon_round.png" />
+        </a>
+        <a href="https://github.com/eddylu94" target="_blank">
+            <img id="socialIcon" alt="GitHub" src="gh-icon_round.png" />
+        </a>
+    </div>
+</div>
+
 <div id="header">
     <div id="header_bar">
         <div id="header_wrapper">
-            <a href="index.php">
+            <img id="menuIcon" alt="Menu" src="menu_icon.png" onclick="toggleFlyout()" />
+            <a id="title_wrapper" href="index.php">
                 <div id="title">
                     <div id="title_left">Eddy&nbsp;</div><div id="title_right">Lu</div>
                 </div>
