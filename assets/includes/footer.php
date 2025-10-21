92c42778adcb0953b79238acc821b476e066d50d
Eddy Lu <eddylu94@users.noreply.github.com>
2025-10-20 22:46:21 -0700

    Use absolute path for statistics page link

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-  &copy; <span id="footer_year"></span> Eddy Lu | <a href="statistics.php">Statistics</a>
+  &copy; <span id="footer_year"></span> Eddy Lu | <a href="/statistics.php">Statistics</a>
   <script>
     var footerYear = document.getElementById("footer_year");
     footerYear.innerHTML = (new Date()).getFullYear();
